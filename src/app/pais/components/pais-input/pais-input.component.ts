ef05723fc4aea2396d25a46afbae0b3d03c93bf1
Marcos <marcos.ledesma.picazo@cieep.com>
2021-03-15 21:41:26 +0100

    Para barra de búsqueda -> onDebounde() funcionando

---
+++ src/app/pais/components/pais-input/pais-input.component.ts
@@ -0,0 +1,45 @@
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-pais-input',
+  templateUrl: './pais-input.component.html',
+  styles: [
+  ]
+})
+export class PaisInputComponent implements OnInit {
+
+  @Output() onEnter: EventEmitter<string> = new EventEmitter();
+  @Output() onDebounce: EventEmitter<string> = new EventEmitter();
+
+  debouncer: Subject<string> = new Subject();  // Emitirlo al dejar de escribir
+
+  termino: string = '';
+
+
+  ngOnInit() {
+    this.debouncer
+      .pipe( debounceTime( 300 ) )
+      .subscribe( valor => {
+        this.onDebounce.emit( valor );
+      });
+  }
+
+
+  buscar(){
+    this.onEnter.emit( this.termino );
+  }
+
+  /*
+  teclaPresionada( event: any ){
+    const valor = event.target.value;
+    console.log( valor );
+  }
+   */
+
+  teclaPresionada(){
+    this.debouncer.next( this.termino );
+  }
+
+}
---
+++ src/app/pais/services/pais.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Country } from '../interfaces/pais.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PaisService {
+
+  private apiUrl: string = 'https://restcountries.eu/rest/v2';
+  //private porNombre: string = 'https://restcountries.eu/rest/v2/name';
+
+  constructor( private http: HttpClient ) { }
+
+  buscarPais( termino: string ): Observable<Country[]> {
+    const url = `${ this.apiUrl }/name/${ termino }`;
+    return this.http.get<Country[]>( url );
+
+      /*
+      .pipe(
+        catchError( err => of([]) )
+      );
+       */
+  }
+
+}
